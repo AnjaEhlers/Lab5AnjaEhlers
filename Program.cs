9eb48045014fe9ee65f40d739990664f4b36ac6d
Anja Ehlers <AnjaEhlers.bittygurl11@gmail.com>
2022-10-13 16:46:03 -0600

    commit message

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿void Menu(){
+    Console.WriteLine("Choose a number, whether to calculate two numbers of your choice, speak to me, or see a cat.");
+    Console.WriteLine("1. use addition. 2. use subtraction 3. use multiplication 4. use division 5. cat picture 6. tell me something 7. exit");
+    string response = Console.ReadLine();
+    if(response == "1")
+    additionFunction();
+    if(response == "2")
+    subtractionFunction();
+    if(response == "3")
+    multiplicationFunction();
+    if(response == "4")
+    divisionFunction();
+    if(response == "5")
+    kitty();
+    if(response == "6")
+    talkToMe();
+    if(response == "7"){
+    Console.WriteLine("Thank you hasta luego");
+    System.Environment.Exit(0);
+    }
+else{
+Menu();
+}
+}
+Menu();
+void additionFunction(){
+    Console.Write("Choose a number.");
+    int userNumberOneAddition=int.Parse(Console.ReadLine());
+    Console.Write("Choose another number.");
+    int userNumberTwoAddition=int.Parse(Console.ReadLine());
+    int additionResult=userNumberOneAddition + userNumberTwoAddition;
+    Console.WriteLine(additionResult);
+}
+void subtractionFunction(){
+    Console.Write("Choose a number.");
+    int userNumberOneSubtraction=int.Parse(Console.ReadLine());
+    Console.Write("Choose another number.");
+    int userNumberTwoSubtraction=int.Parse(Console.ReadLine());
+    int subtractionResult=userNumberOneSubtraction - userNumberTwoSubtraction;
+    Console.WriteLine(subtractionResult);
+}
+void multiplicationFunction(){
+    Console.Write("Choose a number.");
+     int userNumberOneMultiplication=int.Parse(Console.ReadLine());
+     Console.Write("Choose another number.");
+     int userNumberTwoMultiplication=int.Parse(Console.ReadLine());
+     int multiplicationResult=userNumberOneMultiplication * userNumberTwoMultiplication;
+     Console.WriteLine(multiplicationResult);
+}
+void divisionFunction(){
+    Console.Write("Choose a number.");
+    int userNumberOneDivision=int.Parse(Console.ReadLine());
+    Console.Write("Choose another number.");
+    int userNumberTwoDivision=int.Parse(Console.ReadLine());
+    int divisionResult=userNumberOneDivision / userNumberTwoDivision;
+    Console.WriteLine(divisionResult);
+}
+void kitty()
+{
+    Console.WriteLine("<(^..^)>");
+}
+void talkToMe(){
+    Console.WriteLine("Talk to me.");
+    string response = Console.ReadLine();
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab5AnjaEhlers
+1. Create a remote repo named Lab5FirstnameLastname
+2. Clone the remote repo to your local computer
+3. Create a c# project using dotnet new console
+4. Create a void function that displays  a cat on the console
+5. Create a function that asks for user imput and returns the user imput
+6. Create a function that takes two numbers as paramaters adds them together and returns the sum of the two numbers.
+7. Create a function that takes two numbers as paramaters subtracts them and returns the difference of the two numbers.
+8. Create a function that takes two numbers as paramaters multiplies them together and returns the product of the two numbers.
+9. Create a function that takes two numbers as paramaters, Divides them together and returns the quotent of the two numbers.
+10. Create a driver program "menu" to let the user choose what calculation they want to make.
+11. Push your local repo to GitHub.
+12. Submit the repo URL to canvas.
